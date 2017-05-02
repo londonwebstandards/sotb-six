db3a9ec78d1e1c60ded982ff94781e47def25dfc
Matteo Pescarin <matteo.pescarin@londonwebstandards.org>
2017-05-02 14:17:48 +0100

    fixed small eslint errors on gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,14 @@
 'use strict';
 
-import plugins  from 'gulp-load-plugins';
-import yargs    from 'yargs';
-import browser  from 'browser-sync';
-import gulp     from 'gulp';
-import panini   from 'panini';
-import rimraf   from 'rimraf';
-import sherpa   from 'style-sherpa';
-import yaml     from 'js-yaml';
-import fs       from 'fs';
+import plugins from 'gulp-load-plugins';
+import yargs from 'yargs';
+import browser from 'browser-sync';
+import gulp from 'gulp';
+import panini from 'panini';
+import rimraf from 'rimraf';
+import sherpa from 'style-sherpa';
+import yaml from 'js-yaml';
+import fs from 'fs';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -19,7 +19,7 @@ const PRODUCTION = !!(yargs.argv.production);
 // Load settings from settings.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
 
-function loadConfig() {
+function loadConfig () {
   let ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
@@ -34,19 +34,19 @@ gulp.task('default',
 
 // Delete the "dist" folder
 // This happens every time a build starts
-function clean(done) {
+function clean (done) {
   rimraf(PATHS.dist, done);
 }
 
 // Copy files out of the assets folder
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
-function copy() {
+function copy () {
   return gulp.src(PATHS.assets)
     .pipe(gulp.dest(PATHS.dist + '/assets'));
 }
 
 // Copy page templates into finished HTML files
-function pages() {
+function pages () {
   return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
     .pipe(panini({
       root: 'src/pages/',
@@ -59,13 +59,13 @@ function pages() {
 }
 
 // Load updated HTML templates and partials into Panini
-function resetPages(done) {
+function resetPages (done) {
   panini.refresh();
   done();
 }
 
 // Generate a style guide from the Markdown content and HTML template in styleguide/
-function styleGuide(done) {
+function styleGuide (done) {
   sherpa('src/styleguide/index.md', {
     output: PATHS.dist + '/styleguide.html',
     template: 'src/styleguide/template.html'
@@ -74,7 +74,7 @@ function styleGuide(done) {
 
 // Compile Sass into CSS
 // In production, the CSS is compressed
-function sass() {
+function sass () {
   return gulp.src('src/assets/scss/app.scss')
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -85,7 +85,7 @@ function sass() {
       browsers: COMPATIBILITY
     }))
     // Comment in the pipe below to run UnCSS in production
-    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+    // .pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
     .pipe($.if(PRODUCTION, $.cssnano()))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/css'))
@@ -94,13 +94,13 @@ function sass() {
 
 // Combine JavaScript into one file
 // In production, the file is minified
-function javascript() {
+function javascript () {
   return gulp.src(PATHS.javascript)
     .pipe($.sourcemaps.init())
     .pipe($.babel({ignore: ['what-input.js']}))
     .pipe($.concat('app.js'))
     .pipe($.if(PRODUCTION, $.uglify()
-      .on('error', e => { console.log(e); })
+      .on('error', e => { console.log(e); }) // eslint-disable-line no-console
     ))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/js'));
@@ -108,7 +108,7 @@ function javascript() {
 
 // Copy images to the "dist" folder
 // In production, the images are compressed
-function images() {
+function images () {
   return gulp.src('src/assets/img/**/*')
     .pipe($.if(PRODUCTION, $.imagemin({
       progressive: true
@@ -117,7 +117,7 @@ function images() {
 }
 
 // Start a server with BrowserSync to preview the site in
-function server(done) {
+function server (done) {
   browser.init({
     server: PATHS.dist, port: PORT
   });
@@ -125,13 +125,13 @@ function server(done) {
 }
 
 // Reload the browser with BrowserSync
-function reload(done) {
+function reload (done) {
   browser.reload();
   done();
 }
 
 // Watch for changes to static assets, pages, Sass, and JavaScript
-function watch() {
+function watch () {
   gulp.watch(PATHS.assets, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
