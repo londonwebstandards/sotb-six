a8a31bf8e43398f888d72e97e5f6ae696e57f6b7
Matteo Pescarin <matteo.pescarin@londonwebstandards.org>
2018-07-16 16:09:42 +0100

    added resampling of images
    - cleaned gitignore
    - added gulp-responsive dependency
    - updated redirects
    - split out image wrap for speaker

--- gulp/images.js
+++ gulp/images.js
@@ -3,15 +3,52 @@ const gulp = require("gulp");
 const cache = require("gulp-cached");
 const imagemin = require("gulp-imagemin");
 const size = require("gulp-size");
+const responsive = require("gulp-responsive");
 
 const imageminPngquant = require("imagemin-pngquant");
 const imageminJpegRecompress = require("imagemin-jpeg-recompress");
 
 const paths = require("../config.json").paths;
 
-function images() {
+function responsiveImages() {
   return gulp
     .src([`${paths.images.src}**/*`])
+    .pipe(cache("images"))
+    .pipe(
+      responsive(
+        {
+          "speakers/*": [
+            {
+              width: 400,
+              rename: {
+                extname: ".jpg"
+              }
+            }
+          ]
+        },
+        {
+          format: "jpeg",
+          // Global configuration for all images
+          // The output quality for JPEG, WebP and TIFF output formats
+          quality: 85,
+          // Use progressive (interlace) scan for JPEG and PNG output
+          progressive: true,
+          // Strip all metadata
+          withMetadata: false,
+          // Do not emit the error when image is enlarged.
+          errorOnEnlargement: false,
+          errorOnUnusedConfig: false,
+          errorOnUnusedImage: false
+        }
+      )
+    )
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(size());
+}
+
+function compress() {
+  return gulp
+    .src([`${paths.images.src}**/*`, `!${paths.images.src}/speakers/**/*`])
     .pipe(cache("images"))
     .pipe(
       imagemin(
@@ -34,5 +71,6 @@ function images() {
 }
 
 module.exports = {
-  build: images
+  compress: compress,
+  responsive: responsiveImages
 };
